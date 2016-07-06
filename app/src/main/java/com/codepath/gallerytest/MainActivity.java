5dac8f969b53108af1c9958c64be3d0754c4022f
Kemley Nieva <kemleynieva@fb.com>
2016-07-06 15:35:32 -0700

    First commit

--- app/src/main/java/com/codepath/gallerytest/MainActivity.java
+++ app/src/main/java/com/codepath/gallerytest/MainActivity.java
@@ -1,6 +1,16 @@
 package com.codepath.gallerytest;
 
 import android.app.Activity;
+
+
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.MediaController;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,6 +19,8 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -18,12 +30,14 @@ import java.io.IOException;
 public class MainActivity extends Activity {
     private static int RESULT_LOAD_VIDEO = 1;
     String imgDecodableString;
+    VideoView vidView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        writeToFile("hi");
+
     }
 
     public void loadImagefromGallery(View view) {
@@ -39,69 +53,41 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
-            // When an Image is picked
             if (requestCode == RESULT_LOAD_VIDEO && resultCode == RESULT_OK
                     && null != data) {
-                // Get the Image from data
 
                 Uri selectedImage = data.getData();
                 String[] filePathColumn = { MediaStore.Video.Media.DATA };
 
-                // Get the cursor
                 Cursor cursor = getContentResolver().query(selectedImage,
                         filePathColumn, null, null, null);
-                // Move to first row
                 cursor.moveToFirst();
 
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                 imgDecodableString = cursor.getString(columnIndex);
                 cursor.close();
-                final VideoView vidView = (VideoView) findViewById(R.id.video_view);
-                //ImageView imgView = (ImageView) findViewById(R.id.imgView);
-                //vidView.setVideoPath(imgDecodableString);
+                vidView = (VideoView) findViewById(R.id.video_view);
+
+                MediaController mediaController = new MediaController(this);
+                mediaController.setAnchorView(vidView);
+
                 vidView.setVideoURI(selectedImage);
 
-                Log.d("URI", selectedImage.toString());
                 vidView.start();
-                // Set the Image in ImageView after decoding the String
-//                imgView.setImageBitmap(BitmapFactory
-//                        .decodeFile(imgDecodableString));
 
-            } else {
-                Toast.makeText(this, "You haven't picked Image",
-                        Toast.LENGTH_LONG).show();
             }
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                    .show();
         }
-
-    }
-
-    private void writeToFile(String data) {
-
-        String FILENAME = "config1";
-        String string = "hello world!";
-
-
-        try {
-            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-            fos.write(string.getBytes());
-            //fos.flush();
-            fos.close();
-        } catch (IOException e) {
+        catch (Exception e) {
             e.printStackTrace();
         }
-//        try {
-//            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput("config.txt", Context.MODE_PRIVATE));
-//            outputStreamWriter.write(data);
-//            outputStreamWriter.flush();
-//            outputStreamWriter.close();
-//
-//        }
-//        catch (IOException e) {
-//            Log.e("Exception", "File write failed: " + e.toString());
-//        }
+
     }
 
+    public void startAct(View view) {
+        startActivity(new Intent(this, VideoPlayerActivity.class));
+    }
 }
+
+
+
+
